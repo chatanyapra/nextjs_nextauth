b421f04bd99337e73e853a086bf4af96cf4ef2b2
Chatanya pratap <chatanyapra@gmail.com>
2025-05-23 22:22:22 +0530

    creating the file structure for portfolio

---
+++ app/(root)/projects/page.tsx
@@ -0,0 +1,13 @@
+// import ProjectCard from "@/components/ProjectCard";
+
+export default function ProjectsPage() {
+  return (
+    <main className="px-4 py-8">
+      <h1 className="text-4xl font-bold mb-6">My Projects</h1>
+      {/* Map through project data here */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {/* <ProjectCard /> */}
+      </div>
+    </main>
+  );
+}
